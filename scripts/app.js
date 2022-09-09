4a722e5e4f0c7c7f90dea8a5ff4d883816ace3ad
bryancurtis <curtispro97@gmail.com>
2022-09-09 02:41:10 -0400

    updated bio

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const readMore = function readMore() {
+    const about = document.getElementById("extended");
+    const more = document.getElementById("more")
+
+    more.style.display = "none"
+    about.style.display = "inline";
+}
